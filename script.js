9c74aa56b348101081fca709cfe5feb664291ce4
Osvaldo Jimenez <osvcs40@gmail.com>
2021-05-19 13:31:01 -0500

    hide on load working for works!!!

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-window.onload = hideWorkContent;
+// window.onload = hideWorkContent;
 
-function hideWorkContent () {
-    var div = document.getElementById('worksList');
-    var divs = div.getElementsByTagName('myWork');
-    var divArray = [];
-    for (var i = 0; i < divs.length; i += 1) {
-        divArray.push(divs[i].innerHTML);
-    }
-    for (var i = 0; i < divArray.length; i += 1) {
-        divArray[i].style.display = "none";
-    }
-}
+// function hideWorkContent () {
+//     var div = document.getElementById('worksList');
+//     var divs = div.getElementsByTagName('myWork');
+//     var divArray = [];
+//     for (var i = 0; i < divs.length; i += 1) {
+//         divs[i].style.display = "none";
+//         // divArray.push(divs[i].innerHTML);
+//     }
+//     // for (var i = 0; i < divArray.length; i += 1) {
+//     //     divArray[i].style.display = "none";
+//     // }
+// }
 
 function show (element, id) {
-    var workTitle = document.getElementById(id);
+    var workContent = document.getElementById(id);
 
-    if(workTitle.style.display === "none"){
-        workTitle.style.display = "block";
+    if(workContent.style.display !== "block"){
+        workContent.style.display = "block";
         element.style.color = 'black';
     } else {
-        workTitle.style.display = "none";
+        workContent.style.display = "none";
         element.style.color = 'blue';
     }
-}
-
-function sizeUp(element){
-    element.style.display = 5;
-    element.style.color = 'red';
 }
