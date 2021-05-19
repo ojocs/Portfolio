f6b9a36e58021a71630b7e423e4defaafbe93529
Osvaldo Jimenez <osvcs40@gmail.com>
2021-05-18 23:28:07 -0500

    proper fade working!

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+window.onload = hideWorkContent;
+
+function hideWorkContent () {
+
+}
+
 function show (element, id) {
-    if(document.getElementById(id).style.display == "none"){
-        document.getElementById(id).style.display = "block";
+    var workTitle = document.getElementById(id);
+
+    if(workTitle.style.display === "none"){
+        workTitle.style.display = "block";
         element.style.color = 'black';
     } else {
-        document.getElementById(id).style.display = "none";
+        workTitle.style.display = "none";
         element.style.color = 'blue';
     }
 }
